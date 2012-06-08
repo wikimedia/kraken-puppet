ac645c4c1b691a8ed8e762629a22f6cf0fab7848
Andrew Otto <acotto@gmail.com>
2012-06-08 11:59:17 -0400

    Setting map and reduce configs

--- modules/cdh/manifests/hadoop/config.pp
+++ modules/cdh/manifests/hadoop/config.pp
@@ -8,10 +8,18 @@ class cdh::hadoop::config(
 	$dfs_data_dir,
 	$mapred_local_dir,
 	$namenode_hostname,
-	$namenode_port = '8020',
-	$config_directory   = '/etc/hadoop/conf') {
+	$namenode_port                    = '8020',
+	$config_directory                 = '/etc/hadoop/conf',
+	$map_tasks                        = 144,
+	$reduce_tasks                     = 72,
+	$tasktracker_map_tasks_maximum    = 16,
+	$tasktracker_reduce_tasks_maximum = 8
+	) {
 	
 	require cdh::hadoop
+	
+	# TODO: set default map/reduce tasks
+	# automatically based on node stats.
 	
 	file { "$config_directory/core-site.xml":
 		content => template("cdh/hadoop/core-site.xml.erb")
