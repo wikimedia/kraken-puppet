9c0de8e1e248d534f643b635a394d12f8459233e
Andrew Otto <acotto@gmail.com>
2012-06-13 12:30:53 -0400

    Changing default blocksize back to 64MB

--- modules/cdh/manifests/hadoop/config.pp
+++ modules/cdh/manifests/hadoop/config.pp
@@ -13,7 +13,7 @@ class cdh::hadoop::config(
 	$tasktracker_map_tasks_maximum    = 16,
 	$tasktracker_reduce_tasks_maximum = 8,
 	$tasktracker_node_count           = 9,
-	$dfs_block_size                   = 134217728 # 67108864 # 64MB default
+	$dfs_block_size                   = 67108864 # 64MB default
 	) {
 	
 	require cdh::hadoop
