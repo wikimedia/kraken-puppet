6bd9b8215b9181a65e37d4214b95cfcaec4bca27
Andrew Otto <acotto@gmail.com>
2012-06-15 12:27:42 -0400

    Including fabian on analytics cluster, woohoo

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,4 +25,75 @@ alias puptest="sudo puppetd --test --verbose --server analytics1001.wikimedia.or
 alias pupsign="sudo puppetca --vardir /var/lib/puppet.analytics --ssldir /var/lib/puppet.analytics/ssl --confdir=/etc/puppet.analytics sign "',
 		mode => 755,
 	}
+	 
+	include declerambaul
+}
+
+
+
+class declerambaul inherits baseaccount {
+	$username = "declerambaul"
+	$realname = "Fabian Kaelin"
+	$uid = 566
+
+	unixaccount { $realname: username => $username, uid => $uid, gid => $gid }
+
+            if $manage_home {
+                    Ssh_authorized_key { require => Unixaccount[$realname]}
+
+                    ssh_authorized_key {
+			"declerambaul_wiki":
+				ensure	=> present,
+				user	=> $username,
+				type	=> "ssh-rsa",
+				key	=> "AAAAB3NzaC1yc2EAAAABIwAAAQEA3aScUQs0HeQEp/pnMuJ6JKWTwEa8IPVKa7Gkrx2DJ3z1qUtA5wzxSG6m//MJBokIntwBAGuqRazAqs5cB3m4GVqViA1fabxZy55l1/GB+962/d8goVEbtkj/MO47vuUBosVSy5GGjGOs3hWtKId9q6+AU0OCNZwC3j12tXGIX3ztcf4Ef2pdBoCfJMgrvlnIpdFDBftrua9kVvYRQj6tVr5rTbFlEioNgNcdQXhvDP0sU81i1NG/nAeOZMDYOzUscDHa6JcCts3nRyqrqgaMixxGjF7WG42tqS+AEqKi9IOqFnaiHtwipZrnJJ8IxtDOve3HHA3VctBsh03qB4RZ6w==";
+		}
+	}
+}
+
+
+class baseaccount {
+	$enabled = "true"
+
+	Ssh_authorized_key {
+		ensure => $enabled ? {
+			"false" => 'absent',
+			 default => 'present',
+			}
+	}
+}
+
+
+define account_ssh_key($user, $type, $key, $enabled="true") {
+	ssh_authorized_key { $title:
+		user => $user,
+		type => $type,
+		key => $key,
+	}
+}
+
+
+define unixaccount($username, $uid, $gid, $enabled="true") {
+
+
+	if ($myshell) {
+		$shell = $myshell
+	} else {
+		$shell = "/bin/bash"
+	}
+
+	user { "${username}":
+		name		=> $username,
+		uid		=> $uid,
+		gid		=> $gid,
+		comment		=> $title,
+		shell		=> $shell,
+		ensure		=> $enabled ? {
+					"false" => 'absent',
+					default => 'present',
+				},
+		managehome	=> true,
+		allowdupe	=> false,
+		require		=> Group[$gid],
+	}
 }
