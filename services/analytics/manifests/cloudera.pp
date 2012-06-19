1c35e302c3d78796f001f0966e3599ed402c282c
Andrew Otto <acotto@gmail.com>
2012-06-19 11:17:32 -0400

    Setting block size to 128MB.  Will reload all sampled logs with this default block size

--- services/analytics/manifests/cloudera.pp
+++ services/analytics/manifests/cloudera.pp
@@ -39,7 +39,7 @@ class analytics::cloudera::hadoop {
 			"$hadoop_data_directory/i/$hadoop_mapred_local_path",
 			"$hadoop_data_directory/j/$hadoop_mapred_local_path",
 		],
-		# dfs_block_size => "134217728",   # 128MB for testing
+		dfs_block_size => "134217728",   # 128MB for testing
 		# tasktracker_map_tasks_maximum    => 17,
 		# tasktracker_reduce_tasks_maximum => 7,
 	}
