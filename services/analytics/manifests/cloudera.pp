d67b7c6f7cc8b4ec451d04f25095401fc0ff8309
Andrew Otto <acotto@gmail.com>
2012-06-19 15:45:54 -0400

    setting block size to 512MB

--- services/analytics/manifests/cloudera.pp
+++ services/analytics/manifests/cloudera.pp
@@ -39,7 +39,9 @@ class analytics::cloudera::hadoop {
 			"$hadoop_data_directory/i/$hadoop_mapred_local_path",
 			"$hadoop_data_directory/j/$hadoop_mapred_local_path",
 		],
-		dfs_block_size => "268435456", # 256MB "134217728",   # 128MB for testing
+		dfs_block_size => "536870912", # 512MB 
+		# dfs_block_size => "268435456", # 256MB 
+		# dfs_block_size => "134217728", # 128MB
 		# tasktracker_map_tasks_maximum    => 17,
 		# tasktracker_reduce_tasks_maximum => 7,
 	}
