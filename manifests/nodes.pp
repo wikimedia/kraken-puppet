565e1b0998c373d5f05595485e55298d5824575d
Andrew Otto <acotto@gmail.com>
2012-09-17 17:19:33 -0400

    Adding cassandra seed IPs

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -18,9 +18,32 @@ node /^analytics10(0[1-9]|10)/ inherits analytics_basenode {
 		require        => Class["analytics::datastax::apt_source"]
 	}
 
+
+	$cassandra_seeds = [
+		"208.80.154.154",
+		"10.64.21.102",
+		"10.64.21.103",
+		"10.64.21.104",
+		"10.64.21.105",
+		"10.64.21.106",
+		"10.64.21.107",
+		"10.64.21.108",
+		"10.64.21.109",
+		"10.64.21.110"
+	]
+
+	$cassandra_data_file_directories = [
+		"/var/lib/cassandra/data/f",
+		"/var/lib/cassandra/data/g",
+		"/var/lib/cassandra/data/h",
+		"/var/lib/cassandra/data/i",
+		"/var/lib/cassandra/data/j",
+	]
+
 	# configure cassandra.
 	class { "dse::cassandra::server":
 		cluster_name          => $cassandra_data_file_directories,
+		seeds                 => $cassandra_seeds,
 		data_file_directories => $cassandra_cluster_name,
 	}
 }
