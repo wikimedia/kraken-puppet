3b7dec9bff40cf6388672345c6d90b7db068bcd8
Andrew Otto <acotto@gmail.com>
2012-09-18 13:34:24 -0400

    Not subscribing dse service to config files.

--- modules/dse/manifests/cassandra/service.pp
+++ modules/dse/manifests/cassandra/service.pp
@@ -4,7 +4,10 @@ class dse::cassandra::service {
         enable     => true,
         hasstatus  => true,
         hasrestart => true,
-        subscribe  => Class['dse::cassandra::config'],
+        # Not subscribing dse service to 
+        # config files.  I'd rather not have
+        # Cassandra restarted automatically.
+        # subscribe  => Class['dse::cassandra::config'],
         require    => Class['dse::cassandra::config'],
     }
 }
