78cc3cfe691f2120732684da155a3978976a2565
Andrew Otto <acotto@gmail.com>
2012-09-28 15:02:14 -0400

    Installing CDH4 on an02-an22.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,33 +7,49 @@ node analytics_basenode {
 	Exec { environment => 'http_proxy=http://brewster.wikimedia.org:8080' }
 }
 
-# cassandra nodes
-node /^analytics10(0[2-9]|10|18)/ inherits analytics_basenode {
-	# install Datastax Enterprise Cassandra Hadoop
-	include dse
-
-	$cassandra_cluster_name = "KrakenAnalytics"
-
-	$cassandra_seeds = [
-		"10.64.21.103",   # an03
-		"10.64.21.104",   # an04
-	]
-
-	$cassandra_data_file_directories = [
-		"/var/lib/cassandra/data/f",
-		"/var/lib/cassandra/data/g",
-		"/var/lib/cassandra/data/h",
-		"/var/lib/cassandra/data/i",
-		"/var/lib/cassandra/data/j"
-	]
-
-	# configure cassandra.
-	class { "dse::cassandra::server":
-		cluster_name          => $cassandra_cluster_name,
-		seeds                 => $cassandra_seeds,
-		data_file_directories => $cassandra_data_file_directories,
-	}
+# install CDH4 on 20 nodes:
+# analytics1002-1022
+node /^analytics10(0[2-9]|1[0-9]|2[0-2])/ inherits analytics_basenode {
+	# TODO, remove apt_source when we go to production
+	class { "cdh::apt_source": version => 'cdh4' }
+	class { "cdh": require => Class["cdh::apt_source"] }
 }
+
+
+
+
+
+
+
+
+
+# # cassandra nodes
+# node /^analytics10(0[2-9]|10|18)/ inherits analytics_basenode {
+# 	# install Datastax Enterprise Cassandra Hadoop
+# 	include dse
+# 
+# 	$cassandra_cluster_name = "KrakenAnalytics"
+# 
+# 	$cassandra_seeds = [
+# 		"10.64.21.103",   # an03
+# 		"10.64.21.104",   # an04
+# 	]
+# 
+# 	$cassandra_data_file_directories = [
+# 		"/var/lib/cassandra/data/f",
+# 		"/var/lib/cassandra/data/g",
+# 		"/var/lib/cassandra/data/h",
+# 		"/var/lib/cassandra/data/i",
+# 		"/var/lib/cassandra/data/j"
+# 	]
+# 
+# 	# configure cassandra.
+# 	class { "dse::cassandra::server":
+# 		cluster_name          => $cassandra_cluster_name,
+# 		seeds                 => $cassandra_seeds,
+# 		data_file_directories => $cassandra_data_file_directories,
+# 	}
+# }
 
 
 # # analytics1001 is master node (namenode & jobtracker)
--- modules/cdh/manifests/init.pp
+++ modules/cdh/manifests/init.pp
@@ -21,3 +21,27 @@ class cdh {
 	include pig
 	include oozie
 }
+
+
+class cdh::apt_source($version = 'cdh4') {
+	file { "/etc/apt/sources.list.d/cloudera.list":
+		content => "deb http://archive.cloudera.com/debian ${lsbdistcodename}-${version} contrib\ndeb-src http://archive.cloudera.com/debian ${lsbdistcodename}-${version} contrib\n",
+		mode    => 0444,
+		ensure  => 'present',
+	}
+
+	exec { "import_cloudera_apt_key":
+		command   => "/usr/bin/curl -s http://archive.cloudera.com/debian/archive.key | /usr/bin/apt-key add -",
+		require   => Package["curl"],
+		subscribe => File["/etc/apt/sources.list.d/cloudera.list"],
+		unless    => "/usr/bin/apt-key list | /bin/grep -q Cloudera",
+	}
+
+	exec { "apt_get_update_for_cloudera":
+		command => "/usr/bin/apt-get update",
+		timeout => 240,
+		returns => [ 0, 100 ],
+		refreshonly => true,
+		subscribe => [File["/etc/apt/sources.list.d/cloudera.list"], Exec["import_cloudera_apt_key"]],
+	}
+}
