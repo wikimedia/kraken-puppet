96d4c1c58baec6bc4f6a4209787458138e56383c
Andrew Otto <acotto@gmail.com>
2012-10-02 16:01:03 -0400

    Removing requirements on services from config

--- modules/cdh4/manifests/hadoop/config.pp
+++ modules/cdh4/manifests/hadoop/config.pp
@@ -44,22 +44,18 @@ class cdh4::hadoop::config(
 	# automatically based on node stats.
 	file { "$config_directory/core-site.xml":
 		content => template("cdh4/hadoop/core-site.xml.erb"),
-		before  => [Service["namenode"], Service["datanode"]],
 	}
 
 	file { "$config_directory/hdfs-site.xml":
 		content => template("cdh4/hadoop/hdfs-site.xml.erb"),
-		before  => [Service["namenode"], Service["datanode"]],
 	}
 
 	file { "$config_directory/yarn-site.xml":
 		content => template("cdh4/hadoop/yarn-site.xml.erb")
-		before  => [Service["resourcemanager"], Service["nodemanager"]],
 	}
 
 	# only need this to set framework.name
 	file { "$config_directory/mapred-site.xml":
 		content => template("cdh4/hadoop/mapred-site.xml.erb")
-		before  => [Service["resourcemanager"], Service["nodemanager"]],
 	}
 }
