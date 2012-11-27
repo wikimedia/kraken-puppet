4e2bc73c6d99e59e8e09711180cdfa2d4a2a2847
Andrew Otto <acotto@gmail.com>
2012-11-27 13:32:58 -0500

    Requireing hadoop and storm configs before rendering proxy conf

--- manifests/analytics/proxy.pp
+++ manifests/analytics/proxy.pp
@@ -3,6 +3,8 @@
 # internally hosted web services.
 #
 class analytics::proxy {
+	require analytics::hadoop::config, analytyics::storm
+
 	$namenode_hostname = $analytics::hadoop::config::namenode_hostname
 	$frontend_hostname = "analytics1027.eqiad.wmnet"
 	$hue_hostname      = $frontend_hostname
