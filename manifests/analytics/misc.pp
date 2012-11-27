6d062479e67e4e647de8c8de58bf1e55742c579b
Andrew Otto <acotto@gmail.com>
2012-11-27 14:59:42 -0500

    Fixing variable name

--- manifests/analytics/misc.pp
+++ manifests/analytics/misc.pp
@@ -4,8 +4,8 @@ class analytics::misc::web::index {
 	require analytics::hadoop::config, analytics::storm
 	
 	$frontend_hostname = "analytics1027.eqiad.wmnet"
-	$namenode_hostname = $analytics::hadoop::config::namenode_hostnamename
-	$storm_hostname    = $analytics::storm::nimbus_hostname
+	$namenode_hostname = $analytics::hadoop::config::namenode_hostname
+	$storm_hostname    = $analytics::storm::nimbus_host
 	$storm_port        = $analytics::storm::ui_port
 	
 	file { "/var/www/index.php":
